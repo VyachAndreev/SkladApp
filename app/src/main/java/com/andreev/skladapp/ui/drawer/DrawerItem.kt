6734f03e6ee3ee0a4332b8f97d2a1b12a9f4c930
VyachAndreev <60882539+VyachAndreev@users.noreply.github.com>
2021-03-13 16:43:27 +0300

    drawer

---
+++ app/src/main/java/com/andreev/skladapp/ui/drawer/DrawerItem.kt
@@ -0,0 +1,34 @@
+package com.andreev.skladapp.ui.drawer
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.widget.FrameLayout
+import androidx.databinding.DataBindingUtil
+import com.andreev.skladapp.R
+import com.andreev.skladapp.databinding.ViewDrawerItemBinding
+
+class DrawerItem(context: Context, attrs: AttributeSet?) : FrameLayout(context, attrs) {
+
+    private val viewBinding: ViewDrawerItemBinding = DataBindingUtil.inflate(
+        LayoutInflater.from(context),
+        R.layout.view_drawer_item,
+        this,
+        true
+    )
+
+    init {
+        attrs?.let {
+            val arr = context.obtainStyledAttributes(
+                it,
+                R.styleable.DrawerItem
+            )
+            viewBinding.title = arr.getString(R.styleable.DrawerItem_d_title)
+            viewBinding.dividerBottom = arr.getBoolean(
+                R.styleable.DrawerItem_d_dividerBottom,
+                true
+            )
+            arr.recycle()
+        }
+    }
+}
