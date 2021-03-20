e88bb6523ec3daa51484c0203bcd54d5a364eac9
VyachAndreev <60882539+VyachAndreev@users.noreply.github.com>
2021-03-20 15:36:30 +0300

    changed hints logic

---
+++ app/src/main/java/com/andreev/skladapp/ui/_item/HintItem.kt
@@ -0,0 +1,23 @@
+package com.andreev.skladapp.ui._item
+
+import android.view.View
+import com.andreev.skladapp.R
+import com.andreev.skladapp.databinding.ItemSearchHintBinding
+import com.xwray.groupie.viewbinding.BindableItem
+
+class HintItem(val hint: String) : BindableItem<ItemSearchHintBinding>() {
+    override fun getLayout(): Int = R.layout.item_search_hint
+
+    override fun bind(viewBinding: ItemSearchHintBinding, position: Int) {
+        viewBinding.hint = hint
+    }
+
+    override fun initializeViewBinding(view: View): ItemSearchHintBinding =
+        ItemSearchHintBinding.bind(view)
+
+
+    interface OnHintClickedListener {
+        fun onHintClick(hint: String)
+    }
+}
+
--- app/src/main/java/com/andreev/skladapp/ui/search/SearchFragment.kt
+++ app/src/main/java/com/andreev/skladapp/ui/search/SearchFragment.kt
@@ -2,48 +2,85 @@ package com.andreev.skladapp.ui.search
 
 import android.os.Bundle
 import android.view.View
-import android.widget.ArrayAdapter
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.andreev.skladapp.R
 import com.andreev.skladapp.databinding.FragmentSearchBinding
 import com.andreev.skladapp.di.ApplicationComponent
-import com.andreev.skladapp.ui._adapters.HintAdapter
 import com.andreev.skladapp.ui._base.BaseFragment
-import kotlinx.android.synthetic.main.fragment_search.*
+import com.andreev.skladapp.ui._item.HintItem
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.GroupieViewHolder
 import timber.log.Timber
 
 class SearchFragment: BaseFragment<FragmentSearchBinding>(), Observer<String>,
-HintAdapter.ClickListener{
-    lateinit var viewModel: SearchViewModel
+    HintItem.OnHintClickedListener {
 
     override fun getLayoutRes(): Int = R.layout.fragment_search
+
+    lateinit var viewModel: SearchViewModel
 
     override fun injectDependencies(applicationComponent: ApplicationComponent) {
         viewModel = ViewModelProviders.of(this).get(SearchViewModel::class.java)
         viewModel.injectDependencies(applicationComponent)
     }
 
+    lateinit var hintAdapter: GroupAdapter<GroupieViewHolder>
+    lateinit var itemsAdapter: GroupAdapter<GroupieViewHolder>
+
+    private fun initRecyclers() {
+        initHintRecycler()
+        initItemsRecycler()
+    }
+
+    private fun initHintRecycler() {
+        hintAdapter = GroupAdapter()
+        viewBinding.apply {
+            recyclerHints.visibility = View.VISIBLE
+            recyclerHints.apply {
+                adapter = hintAdapter
+                layoutManager = LinearLayoutManager(context)
+            }
+        }
+    }
+
+    private fun initItemsRecycler() {
+        itemsAdapter = GroupAdapter()
+        viewBinding.apply {
+            recycler.apply {
+                adapter = itemsAdapter
+                layoutManager = LinearLayoutManager(context)
+            }
+        }
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         viewBinding.viewModel = viewModel
+        initRecyclers()
         viewBinding.searchBtn.setOnClickListener {
+            hintAdapter.clear()
             Timber.i("editText text = ${viewBinding.searchEt.text}")
         }
         viewModel.searchedText.observe(this, this)
-        viewModel.hints.observe(this, hintsObserver)
+        viewModel.hints.observe(this, hintsListener)
     }
 
     override fun onChanged(searchedText: String?) {
         viewModel.getHints(searchedText)
     }
 
-    private val hintsObserver = Observer<ArrayList<String>> {
-        HintAdapter(context!!, it, this).also{ adapter ->
-            viewBinding.searchEt.setAdapter(adapter)
-        }
+    private val hintsListener = Observer<ArrayList<String>> {hints ->
+        hintAdapter.clear()
+        hintAdapter.addAll(
+            hints.map {
+                HintItem(it)
+            }
+        )
     }
 
-    override fun onClick() {
+    override fun onHintClick(hint: String) {
+        viewModel.searchedText.value = hint
         viewBinding.searchBtn.callOnClick()
     }
 }
