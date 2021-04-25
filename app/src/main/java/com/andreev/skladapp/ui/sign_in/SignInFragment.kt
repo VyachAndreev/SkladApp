3e6873d0b762844c327f939089f12ec3feb87d3a
VyachAndreev <60882539+VyachAndreev@users.noreply.github.com>
2021-04-25 23:03:11 +0300

    layout swap

--- app/src/main/java/com/andreev/skladapp/ui/sign_in/SignInFragment.kt
+++ app/src/main/java/com/andreev/skladapp/ui/sign_in/SignInFragment.kt
@@ -6,17 +6,17 @@ import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.lifecycle.ViewModelProviders
 import com.andreev.skladapp.R
-import com.andreev.skladapp.databinding.TestBinding
+import com.andreev.skladapp.databinding.FragmentSignInBinding
 import com.andreev.skladapp.di.ApplicationComponent
 import com.andreev.skladapp.ui._base.BaseFragment
 import com.andreev.skladapp.ui.hub.HubFragment
 
 
-class SignInFragment : BaseFragment<TestBinding>()/*, Observer<String>*/ {
+class SignInFragment : BaseFragment<FragmentSignInBinding>(){
     lateinit var viewModel: SignInViewModel
     private val URL = "http://ferro-trade.ru/login?logout"
 
-    override fun getLayoutRes(): Int = R.layout.test
+    override fun getLayoutRes(): Int = R.layout.fragment_sign_in
 
     override fun injectDependencies(applicationComponent: ApplicationComponent) {
         viewModel = ViewModelProviders.of(this).get(SignInViewModel::class.java)
@@ -24,21 +24,13 @@ class SignInFragment : BaseFragment<TestBinding>()/*, Observer<String>*/ {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-//        viewBinding.viewModel = viewModel
-//        viewBinding.btnSignIn.setOnClickListener {
-//            hideKeyBoard()
-//            signInUser()
-//        }
-//
-//        viewModel.login.observe(this, this)
-//        viewModel.password.observe(this, this)
         openInWebView(URL)
 
         viewBinding.webView.webViewClient = object : WebViewClient() {
             override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
-                return if (url.startsWith("http://ferro-trade.ru/login")) {
-                    openInWebView("http://ferro-trade.ru/login?error")
-                    return true
+                return return if (url.startsWith("http://ferro-trade.ru/login")) {
+                    openInWebView(url)
+                    true
                 } else {
                     launchFragment(
                         R.id.fragment_container,
@@ -46,7 +38,7 @@ class SignInFragment : BaseFragment<TestBinding>()/*, Observer<String>*/ {
                         false,
                         replace = true,
                     )
-                    return true
+                    true
                 }
             }
         }
@@ -56,32 +48,4 @@ class SignInFragment : BaseFragment<TestBinding>()/*, Observer<String>*/ {
     private fun openInWebView(url: String) {
         viewBinding.webView.loadUrl(url)
     }
-
-
-//    override fun onChanged(string: String?) {
-//        Timber.i("changed")
-//        viewModel.checkSingInAbility()
-//    }
-//
-//    private fun signInUser() {
-//        var isSuccessSignIn: Boolean
-//        scopeMain.launch {
-//            showLoading()
-//            withContext(Dispatchers.IO) {
-//                isSuccessSignIn = viewModel.signUser()
-//            }
-//            hideLoading()
-//
-//            if (isSuccessSignIn) {
-//                launchFragment(
-//                    R.id.fragment_container,
-//                    HubFragment(),
-//                    false,
-//                    replace = true,
-//                )
-//            }  else {
-//                showToast("Something went wrong")
-//            }
-//        }
-//    }
 }
--- app/src/main/java/com/andreev/skladapp/ui/sign_in/SignInViewModel.kt
+++ app/src/main/java/com/andreev/skladapp/ui/sign_in/SignInViewModel.kt
@@ -1,48 +1,12 @@
 package com.andreev.skladapp.ui.sign_in
 
-import androidx.lifecycle.MutableLiveData
 import com.andreev.skladapp.di.ApplicationComponent
-import com.andreev.skladapp.network.repositories.UserRepository
-import com.andreev.skladapp.stored_data.UserStoredData
 import com.andreev.skladapp.ui._base.BaseViewModel
-import timber.log.Timber
-import javax.inject.Inject
 
 class SignInViewModel: BaseViewModel() {
-    @Inject
-    lateinit var userRepository: UserRepository
-
-    @Inject
-    lateinit var userData: UserStoredData
-
-    val login = MutableLiveData<String>()
-    val password = MutableLiveData<String>()
-    val isSignInAvailable = MutableLiveData<Boolean>()
-
-    init {
-        isSignInAvailable.value = false
-    }
 
     override fun injectDependencies(applicationComponent: ApplicationComponent) {
         applicationComponent.inject(this)
-    }
-
-    fun checkSingInAbility() {
-        val login = login.value ?: ""
-        val password = password.value ?: ""
-        isSignInAvailable.value =
-            login.isNotEmpty() && password.isNotEmpty()
-        Timber.i("${isSignInAvailable.value}")
-    }
-
-    suspend fun signUser() : Boolean {
-        val login = login.value ?: return false
-        val password = password.value ?: return false
-        val response = userRepository.signInUser(
-            login, password
-        )
-        userData.saveUser("user")
-        return true
     }
 
 }
